a45d5888a006daa267534c67eddb4b40d00d0fda
eugenehiggins <genotype@gknow.com>
2016-02-11 16:49:14 -0700

    got validation working

--- js/scripts2.js
+++ js/scripts2.js
@@ -5,24 +5,17 @@ $(document).ready(function() {
     // collect all of the fields that are required
     var $required = $('input,textarea,select').filter('[required]');
 
+    $("#submit_button").on("click",function(){
+        consle.log("yo")
+    })
 
+    var addAsterisks = function(){
+        $required.each(function(i,el){
+            console.log($(el).siblings('label').append("*"));
+        })
+    }
 
-    // isEmpty function
-    var isEmpty = function ($fields) {
-
-        this.empty = false;
-        var self = this;
-        //  for each required item check to see if it's empty
-        $fields.each(function(index, el) {
-            //  if it's empty return true
-            if ($(el).val() === ""){
-                self.empty = true;
-            }
-            
-        });
-        //      if not return false
-        return this.empty;
-    };
+    addAsterisks();
 
     // find email input
     var findEmail = function ($fields){
@@ -37,60 +30,81 @@ $(document).ready(function() {
     }
 
     //  isValidEmail function
-    var isValidEmail = function (emailField) {
-        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-        var email = $(emailField).val();
-        return pattern.test(email);
+    var isValidEmail = function (str) {
+
+        //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        var pattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+        //var email = $(emailField).val();
+        return pattern.test(str);
     }
 
-    
+    var fieldIsValid = function(el) {
+
+        switch (el.type) {
+            case "text":
+                if($(el).val() === "") {
+                    console.log("text false")
+                    return false;
+                }
+                break;
+            case "email":
+                if($(el).val() === "" || !isValidEmail($(el).val())) {
+                    console.log("email false")
+                    return false;
+                }
+                break;
+            case "select-one":
+                console.log($(el).val());
+                if($(el).val() === "Select Choice") {
+                    return false;
+                }
+        }
+
+        return true;
+    }
+
+    var formIsValid = function (evt) {
+        this.isValid = false;
+        var self = this;
+
+        // check to see if ANY fields are empty, if they are, invalidate form
+        $required.each(function(i, el){
+            if(!fieldIsValid(el)){
+                self.isValid = false;
+                return false;
+            } else {
+                self.isValid = true;
+            };
+
+        });
+
+        return this.isValid;
+
+    }
+
     // check each keystroke in required fields to see if 
     // the error style should be removed.
     $required.bind('input', function(event) {
+        if(formIsValid(event)){
+
+
+            $submitButton.prop('disabled', false);
+        } else {
+            $submitButton.prop('disabled', true);
+        };
 
         $target = $(event.target);
         $parent = $target.parent();
 
-
-        if($target.val() !== "") {
-            console.log($target.parent());
+        if(fieldIsValid(event.target)){
             $parent.removeClass('has-error')
                 .addClass('has-success');
-
+            $target.after("<span class=\"glyphicon glyphicon-ok form-control-feedback\" aria-hidden=\"true\"></span> <span class=\"sr-only\">(success)</span>");
         } else {
             $parent.addClass('has-error')
-                .removeClass('has-success')
-            console.log('empty');
+                .removeClass('has-success');
+            $target.after("<span class=\"glyphicon glyphicon-error form-control-feedback\" aria-hidden=\"true\"></span> <span class=\"sr-only\">(error)</span>");
         }
-        //$parent.addClass('has-success');
     });
 
-    var theEmail = findEmail($required);
-
-    //  when a required field loses focus
-    $required.on('blur', function(event) {
-        var $target = $(event.target);
-        //  get email field
-        var emailField = findEmail($required);
-
-
-        //  if any required fields are empty, or if email is malformed, 
-        if (isEmpty($required) === true || isValidEmail(emailField) === false ) {
-            //make submit button inactive
-            $submitButton.prop('disabled', true);
-            
-            // change style of input to reflect bootstrap valid state
-   
-            // var errorTag = "<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>"
-            // $target.parent()
-            //     .addClass('has-error has-feedback')
-            //     .append(errorTag);
-
-        } else {
-            console.log($submitButton);
-            $submitButton.prop('disabled', false);
-        }
-
-    //  when submit button is clicked, submit form
-    });
 });
