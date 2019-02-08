50579b298b0a89aeae090ae9f61d72899afe3fb7
Gene Higgins <ehigginsiii@cnm.edu>
2019-02-08 15:45:42 -0700

    behavior for , behavior for  and of not found request not found fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OpieFormComponent } from './opie-form/opie-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { OpieFormComponent } from './opie-form/opie-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/opie-form/opie-form.component.ts
+++ src/app/opie-form/opie-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import * as td from 'tempusdominus-bootstrap-4';
+import * as moment from 'moment';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-opie-form',
@@ -8,7 +9,10 @@ import * as td from 'tempusdominus-bootstrap-4';
   styleUrls: ['./opie-form.component.scss']
 })
 export class OpieFormComponent implements OnInit {
+  dateNeededDate: NgbDateStruct ;
   opieForm: FormGroup;
+  showExplainOther = false;
+
   recurring = [
     {'id': 'no-answer', 'name': 'No choice'},
     {'id': 'one-time', 'name': 'One time'},
@@ -21,6 +25,24 @@ export class OpieFormComponent implements OnInit {
     {'id': 'Annually', 'name': 'Annually'},
     {'id': 'Other', 'name': 'Other'},
   ];
+
+  type_of_request = [
+    {'id': 'No Choice', name: 'No Choice'},
+    {'id': 'Accreditation Reporting', name: 'Accreditation Reporting'},
+    {'id': 'Institutional Research', name: 'Institutional Research'},
+    {'id': 'Learning Assessment', name: 'Learning Assessment'},
+    {'id': 'Performance Improvement', name: 'Performance Improvement'},
+    {'id': 'Unknown', name: 'Unknown'},
+  ]
+
+  pi_category = [
+    {'id': 'No Choice', name: 'No Choice'},
+    {'id': 'Department Profile', name: 'Department Profile'},
+    {'id': 'PI Report', name: 'PI Report'},
+    {'id': 'Resources', name: 'Resources'},
+    {'id': 'Survey', name: 'Survey'},
+    {'id': 'Training', name: 'Training'},
+  ]
 
 
 
@@ -37,21 +59,30 @@ export class OpieFormComponent implements OnInit {
       TITLE: ['', Validators.required],
       First__bName: ['', Validators.required],
       Last__bName: ['', Validators.required],
-      Email__bAddress: ['', Validators.required],
+      Email__bAddress: ['', [Validators.required, Validators.email]],
 
+      dateNeeded: ['' , Validators.required],
       One__uTime__bor__bRecurring__Q: [this.recurring[0].id , Validators.required],
       Recurring__bSchedule: [this.recurring_schedule[0].id],
       Please__bExplain__bOther: [''],
-      // Please__bExplain__bOther: [''],
+      Type__bof__bRequest: [this.type_of_request[0].id],
+      PI__bCategory: [''],
+      If__bUnknown__bPlease__bDescribe__c: [''],
     });
 
     this.onChanges();
+    const dateInTwoWeeks = moment().add(2, 'weeks');
+    this.dateNeededDate = { year: dateInTwoWeeks.year(), month: dateInTwoWeeks.month() + 1, day: dateInTwoWeeks.date()};
+    this.opieForm.get('dateNeeded').setValue(this.dateNeededDate);
 
   }
 
   onChanges(): void {
     this.opieForm.get('One__uTime__bor__bRecurring__Q').valueChanges.subscribe(val => {
       console.log(val);
+    });
+    this.opieForm.get('Recurring__bSchedule').valueChanges.subscribe(val => {
+      this.showExplainOther = val === 'Other';
     });
   }
 
