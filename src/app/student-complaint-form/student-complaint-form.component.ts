f9814cdbd2deb922408c99c407c8a1472731a57a
Brooks Walch <bwalch@cnm.edu>
2019-03-06 16:28:41 -0700

    updates required validation and separates date

--- src/app/student-complaint-form/student-complaint-form.component.ts
+++ src/app/student-complaint-form/student-complaint-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-student-complaint-form',
@@ -8,16 +9,18 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class StudentComplaintFormComponent implements OnInit {
 
+  node;
+  dateOccurred: NgbDateStruct;
   studentComplaintForm: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.studentComplaintForm = this.fb.group({
-      First__bName: [ '' ],
-      Last__bName: [ '' ],
-      Email__bAddress: [ '' ],
-      User__bName: [ '' ],
+      First__bName: [ '', [ Validators.required ] ],
+      Last__bName: [ '', [ Validators.required ] ],
+      Email__bAddress: [ '', [ Validators.required ] ],
+      User__bName: [ '', [ Validators.required] ],
       CNM__bPhone: [ '' ],
       CNM__bID__bNumber: [ '' ],
       Student__bAddress: [ '' ],
@@ -27,7 +30,7 @@ export class StudentComplaintFormComponent implements OnInit {
       TITLE: [ '' ],
       Feedback: [ '' ],
       Feedback__bCategory: [ '' ],
-      dateOccurred: [ '' ],
+      dateOccurred: [ '', [ Validators.required ] ],
       Student__bStatus: [ '' ],
       Course__bName: [ '' ],
       Name__bof__bIndividual: [ '' ],
@@ -35,15 +38,44 @@ export class StudentComplaintFormComponent implements OnInit {
       Referred__bby: [ '' ],
       LONGDESCRIPTION: [ '' ],
       Individual__bOutcome__bor__bDiscussion: [ '' ],
-      Month_Date__bEvent__bOccured: [ '' ],
-      Day_Date__bEvent__bOccured: [ '' ],
-      Year_Date__bEvent__bOccured: [ '' ],
+      Month_Date__bEvent__bOccurred: [ '' ],
+      Day_Date__bEvent__bOccurred: [ '' ],
+      Year_Date__bEvent__bOccurred: [ '' ],
       TO: [ 'studentcomp@cnm.edu' ],
       FROM: [ 'Web@cnm.edu' ],
       PROJECTNUM: [ '21' ],
       PROJECTNAME: [ 'Dean of Students' ],
       DATE_TYPE: [ '0' ],
     });
+    this.onChanges();
   }
+
+  onChanges(): void {
+    // this.studentComplaintForm.get('One__uTime__bor__bRecurring__Q').valueChanges.subscribe(val => {
+      // console.log(val);
+    // });
+    // this.studentComplaintForm.get('Recurring__bSchedule').valueChanges.subscribe(val => {
+      // this.showExplainOther = val === 'Other';
+      // console.log(val);
+    // });
+    // this.studentComplaintForm.get('Strategic__bgoal__bto__bwhich__bthis__brelates').valueChanges.subscribe(val => {
+      // this.studentComplaintForm.controls[ 'Strategic__binitiative__bto__bwhich__bthis__brelates' ].setValue(this.strategicInitiative[ val ][ 0 ], {onlySelf: true});
+      // console.log(val);
+      // console.log(this.strategicInitiative[ val ]);
+    // });
+    this.studentComplaintForm.get('dateOccurred').valueChanges.subscribe(val => {
+      // console.log(val)
+      this.studentComplaintForm.get('Month_Date__bEvent__bOccurred').setValue(val.month);
+      this.studentComplaintForm.get('Day_Date__bEvent__bOccurred').setValue(val.day);
+      this.studentComplaintForm.get('Year_Date__bEvent__bOccurred').setValue(val.year);
+    });
+  
+  }
+
+  onSubmit = () => {
+    console.warn(this.studentComplaintForm.value);
+    // this.http.post('https://sos.cnm.edu/MRcgi/MRProcessIncomingForms.pl')
+    //   .subscribe();
+  };
 
 }
